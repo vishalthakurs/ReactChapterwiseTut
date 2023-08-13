0be437e8c5b38ee8c1e8fc9c80ba3e387cb71e9c
vishalthakurs <vishalsingh041@gmail.com>
2023-08-13 19:17:19 +0530

    getting data from seperate file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import './App.css';
 import Video from './components/Video';
+import data from './data/data';
 
 function App() {
-  let data = [{
-    id:"1",
-    title: 'React JS tutorial',
-    views: '999K',
-    time: '1 year ago',
-    channel: 'Vishal Dev',
-    verified:"false"
-  },
-  {
-    id:"2",
-    title: 'Node JS tutorial',
-    views: '100K',
-    time: '1 month ago',
-    channel: 'Vishal Dev',
-    verified:"false"
-  },
-  {
-    id:"3",
-    title: 'Mongo DB tutorial',
-    views: '1M',
-    time: '1 year ago',
-    channel: 'Vishal Dev',
-    verified:"false"
-  }];
+  
   return (
     <div className="App">
       <div>Videos</div>
---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+const data = [{
+    id:"1",
+    title: 'React JS tutorial',
+    views: '999K',
+    time: '1 year ago',
+    channel: 'Vishal Dev',
+    verified:"false"
+  },
+  {
+    id:"2",
+    title: 'Node JS tutorial',
+    views: '100K',
+    time: '1 month ago',
+    channel: 'Vishal Dev',
+    verified:"false"
+  },
+  {
+    id:"3",
+    title: 'Mongo DB tutorial',
+    views: '1M',
+    time: '1 year ago',
+    channel: 'Vishal Dev',
+    verified:"false"
+  }];
+
+  export default data;
