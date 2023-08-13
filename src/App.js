4ba0cd68f6d88b8194b5865651cc24a83bde4ccf
vishalthakurs <vishalsingh041@gmail.com>
2023-08-13 18:39:20 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Video from "./components/Video";
+//import Video from "./components/Video";
 import './App.css'
+import Resume from "./Assignments/Assignment1/Resume";
 function App(){
   return ( 
+  // <div className="App">
+  //     Hello This is App
+  //     <Video title="React Js Tutorials" tutor="Vishal" views="19.2M" time={Date.now} />
+  //     <Video title="Node Js Tutorials" tutoe="Janny" views="19.2M" time={Date.now}  />
+  // </div>
   <div className="App">
-      Hello This is App
-      <Video title="React Js Tutorials" tutor="Vishal" views="19.2M" time={Date.now} />
-      <Video title="Node Js Tutorials" tutoe="Janny" views="19.2M" time={Date.now}  />
+    <Resume />
   </div>
   )
 
