44a856e50ae97b86d60a10c7856f931f8f7d4bee
vishalthakurs <vishalsingh041@gmail.com>
2023-08-13 21:09:49 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.css';
+import PlayButton from './components/PlayButton';
 import Video from './components/Video';
 import data from './data/data';
-
 function App() {
-  
   return (
-    <div className="App">
+    <div className="App" onClick={()=>console.log('App')}>
       <div>Videos</div>
-      {
-        data.map(ele=><Video
-          key={ele.id}
-          title={ele.title}
-          views={ele.views}
-          time={ele.time}
-          channel={ele.channel}
-          verified={ele.verified} />)
-      }
+      {data.map((video) => (
+        <Video
+          key={video.id}
+          title={video.title}
+          views={video.views}
+          time={video.time}
+          channel={video.channel}
+          verified={video.verified}
+          id={video.id}
+        >
+          <PlayButton
+            onPlay={() => console.log('Playing..',video.title)}
+            onPause={() => console.log('Paused..',video.title)}
+          >
+            {video.title}
+          </PlayButton>
+        </Video>
+      ))}
+
+      <div style={{ clear: 'both' }}>
+        {/* <PlayButton message="play-msg" onPlay={()=>console.log('Play')} onPause={()=>console.log('Pause')}>Play</PlayButton> */}
+
+        {/* <PlayButton message="pause-msg" onSmash={()=>alert('Playyy')}>Pause</PlayButton> */}
+      </div>
     </div>
   );
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,18 +1,21 @@
 import './Video.css';
 
-function Video({title,channel="Coder Dost",views,time,verified}) {
+function Video({title,id,channel="Coder Dost",views,time,verified,children}) {
+   
+  
   return (
       <>
       <div className='container'>
       <div className="pic">
-      <img src="https://i.imgur.com/yXOvdOSs.jpg" alt="Katherine Johnson" />
+      <img src={`https://picsum.photos/id/${id}/160/90`} alt="Katherine Johnson" />
       </div>
       <div className="title">{title}</div>
-      {/* {verified?<div className="channel">{channel}✔</div>:<div className="channel">{channel}</div>} */}
-      <div className="channel">{channel} {verified && '✔'}</div>
-      
+      <div className="channel">{channel} {verified && '✅'} </div>
       <div className="views">
         {views} views <span>.</span> {time}
+      </div>
+      <div>
+        {children}
       </div>
       </div>
       </>
